b2d4a2a1ff50b34eb2701c4a89eb629cbe0c250e
KholoudEbrahim <khokoudebrahim419@gmail.com>
2025-02-03 19:40:49 +0200

    Add project files.

---
+++ Exam-project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exam_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
